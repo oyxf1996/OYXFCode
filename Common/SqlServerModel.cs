f4bcd11b3607e3041f88469853626b35f148e3cf
oyxf <17805966552@163.com>
2019-11-27 22:33:15 +0800

    ado.net封装DbHelperSqlServer访问帮助类

---
+++ Common/SqlServerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SqlServerModel
+    {
+        public string sql;
+
+        public SqlParameter[] parameters;
+
+        public SqlServerModel(string sql, SqlParameter[] parameters)
+        {
+            this.sql = sql;
+            this.parameters = parameters;
+        }
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
