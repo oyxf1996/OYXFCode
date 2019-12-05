a629842e51dcac9771e6b1bc535ffc2502c5762d
oyxf <17805966552@163.com>
2019-12-05 20:45:47 +0800

    ado.net封装DbHelperSqlite访问帮助类

---
+++ Common/SqliteModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SqliteModel
+    {
+        public string sql;
+
+        public SQLiteParameter[] parameters;
+
+        public SqliteModel(string sql, SQLiteParameter[] parameters)
+        {
+            this.sql = sql;
+            this.parameters = parameters;
+        }
+
+    }
+}
