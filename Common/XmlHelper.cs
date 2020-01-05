b64b9c1dace0daf1d3733ee02cbe86314577b339
oyxf <17805966552@163.com>
2020-01-05 12:14:02 +0800

    xml操作

---
+++ Common/XmlHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Common
+{
+    public class XmlHelper
+    {
+        public static void CreateXml()
+        {
+            string rootName = "Books";
+            string xmlPath = @"C:\Users\admin\Desktop\test\Books.xml";
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlDeclaration xmlDec = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null);
+            xmlDoc.AppendChild(xmlDec);
+
+            XmlElement rootElement = xmlDoc.CreateElement(rootName);
+            xmlDoc.AppendChild(rootElement);
+
+            XmlElement bookElement1 = xmlDoc.CreateElement("Book");
+            bookElement1.SetAttribute("Number", "1");
+            rootElement.AppendChild(bookElement1);
+            XmlElement bookNameElement1 = xmlDoc.CreateElement("BookName");
+            bookNameElement1.InnerText = "《论语》";
+            bookElement1.AppendChild(bookNameElement1);
+            XmlElement bookPriceElement1 = xmlDoc.CreateElement("BookPrice");
+            bookPriceElement1.InnerText = "13.55";
+            bookElement1.AppendChild(bookPriceElement1);
+            XmlElement bookAuthorElement1 = xmlDoc.CreateElement("BookAuthor");
+            bookAuthorElement1.InnerText = "孔子";
+            bookElement1.AppendChild(bookAuthorElement1);
+
+            xmlDoc.Save(xmlPath);
+        }
+        
+        public static void CreateXml2()
+        {
+            string xmlPath = @"C:\Users\admin\Desktop\test\Books.xml";
+
+            XmlDocument xmlDoc = new XmlDocument();
+            
+            StringBuilder content = new StringBuilder();
+            String newLine = Environment.NewLine;
+            content.Append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>" + newLine);
+            content.Append("<Books>" + newLine);
+            content.Append("</Books>");
+            xmlDoc.LoadXml(content.ToString());
+
+            XmlElement root = xmlDoc.SelectSingleNode("Books") as XmlElement;
+
+            XmlElement bookElement1 = xmlDoc.CreateElement("Book");
+            bookElement1.SetAttribute("Number", "1");
+            root.AppendChild(bookElement1);
+            XmlElement bookNameElement1 = xmlDoc.CreateElement("BookName");
+            bookNameElement1.InnerText = "《孟子》";
+            bookElement1.AppendChild(bookNameElement1);
+            XmlElement bookPriceElement1 = xmlDoc.CreateElement("BookPrice");
+            bookPriceElement1.InnerText = "19.88";
+            bookElement1.AppendChild(bookPriceElement1);
+            XmlElement bookAuthorElement1 = xmlDoc.CreateElement("BookAuthor");
+            bookAuthorElement1.InnerText = "孟子";
+            bookElement1.AppendChild(bookAuthorElement1);
+
+            xmlDoc.Save(xmlPath);
+        }
+        
+        public static void AppendXml()
+        {
+            string xmlPath = @"C:\Users\admin\Desktop\test\Books.xml";
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true; //忽略文档里面的注释
+
+            XmlReader reader = XmlReader.Create(xmlPath, settings);
+            xmlDoc.Load(reader);
+            reader.Close();
+
+            XmlElement root = xmlDoc.SelectSingleNode("Books") as XmlElement;
+
+            XmlElement bookElement1 = xmlDoc.CreateElement("Book");
+            bookElement1.SetAttribute("Number", "2");
+            root.AppendChild(bookElement1);
+            XmlElement bookNameElement1 = xmlDoc.CreateElement("BookName");
+            bookNameElement1.InnerText = "《孟子》";
+            bookElement1.AppendChild(bookNameElement1);
+            XmlElement bookPriceElement1 = xmlDoc.CreateElement("BookPrice");
+            bookPriceElement1.InnerText = "19.88";
+            bookElement1.AppendChild(bookPriceElement1);
+            XmlElement bookAuthorElement1 = xmlDoc.CreateElement("BookAuthor");
+            bookAuthorElement1.InnerText = "孟子";
+            bookElement1.AppendChild(bookAuthorElement1);
+
+            xmlDoc.Save(xmlPath);
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using ADO.NET;
 using Common;
 
@@ -15,7 +16,39 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //XmlDocument xmldoc = new XmlDocument();
+            ////3.创建第一行描述信息，添加到xmldoc文档中
+            //XmlDeclaration xmldec = xmldoc.CreateXmlDeclaration("1.0", "utf-8", null);
+            //xmldoc.AppendChild(xmldec);
+            ////4.创建根节点，xml文档有且只能有一个根节点
+            //XmlElement xmlele1 = xmldoc.CreateElement("Books");
+            ////5.将根节点添加到xmldoc文档中
+            //xmldoc.AppendChild(xmlele1);
+            ////6.创建子节点
+            //XmlElement xmlele2 = xmldoc.CreateElement("Book");
+            ////7.将子节点添加到根节点
+            //xmlele1.AppendChild(xmlele2);
+            ////6.将子节点添加到子节点
+            //XmlElement name = xmldoc.CreateElement("Name");
+            //name.InnerText = "c#从入门到精通";
+            //xmlele2.AppendChild(name);
+            //XmlElement author = xmldoc.CreateElement("Author");
+            //author.InnerText = "Holliszzz";
+            //xmlele2.AppendChild(author);
+            //XmlElement price = xmldoc.CreateElement("Price");
+            //price.InnerText = "99";
+            //xmlele2.AppendChild(price);
 
+            ////属性
+            //XmlElement xmlele3 = xmldoc.CreateElement("Pen");
+            //xmlele1.AppendChild(xmlele3);
+            //XmlElement item = xmldoc.CreateElement("Ttem");
+            //item.SetAttribute("材质", "金子");
+            //item.SetAttribute("颜色", "黄色");
+            //xmlele3.AppendChild(item);
+
+            //xmldoc.Save("Books.xml");
+            XmlHelper.CreateXml2();
         }
     }
 }
